c0bbcbcdddb98160305b54c567ab90904353ea4d
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-10-19 21:54:42 +0200

    Create Counter.php

---
+++ Counter/source/Counter.php
@@ -0,0 +1,23 @@
+<?php
+namespace tarik;
+class Counter
+{
+    private $numbers = array();
+    public function getCounter()
+    {
+        $result = '';
+        foreach ($this->numbers as $number => $count){
+            $result .= "$number: $count, ";
+        }
+        $result= substr($result, 0, -2);
+        return $result;
+    }
+    public function count($number)
+    {
+        if(isset($this->numbers[$number])){
+            $this->numbers[$number]++;
+        }else{
+            $this->numbers[$number] = 1;
+        }
+    }
+}
